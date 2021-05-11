0a94498c1d50b5e5bcba71bbc772713977b27529
GorgetGuilhem <gorgetguilhem@gmail.com>
2021-05-11 14:45:26 +0200

    Externalized cron management

---
+++ manifests/cron.pp
@@ -0,0 +1,25 @@
+# @summary
+# Creates the cron configuration for borgmatic
+#
+define borgmatic::cron (
+  Optional[String] $cron_command                                              = '/root/.local/bin/borgmatic',
+  Optional[String] $cron_environment                                          = undef,
+  Optional[String] $cron_user                                                 = 'root',
+  Optional[String] $cron_minute                                               = '*',
+  Optional[String] $cron_hour                                                 = '*',
+  Optional[String] $cron_weekday                                              = '*',
+  Optional[String] $cron_monthday                                             = '*',
+  Optional[String] $cron_month                                                = '*'
+){
+  cron { 'borgmatic':
+    ensure      => present,
+    command     => $cron_command,
+    environment => $cron_environment,
+    user        => $cron_user,
+    minute      => $cron_minute,
+    hour        => $cron_hour,
+    weekday     => $cron_weekday,
+    monthday    => $cron_monthday,
+    month       => $cron_month
+  }
+}
